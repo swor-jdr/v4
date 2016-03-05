c7eee7cca4b35f4800f9b8681a1ff2e6fd9819b3
Tak Castel <takcastel@gmail.com>
2016-03-05 21:40:34 +0100

    test view more topics fix 2

--- ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
+++ ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
@@ -5,21 +5,9 @@ head.ready(function () {
 
 		var hack = new Date();
 	
-		$("#fltl").load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #fltl', function() {
-			console.log('ok');
+		$("#rtblock").load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #fltl', function() {
+		}, function(err) {
+			console.error(err);
 		});
-
-		// $.ajax({
-		// 	url: 'app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()),
-			
-		// 	context: document.getElementById("fltl"),
-		// 	error: function (e, text, ee) {
-		// 	},
-		// 	success: function (s, x) {
-		// /* sucks, but a bug in phpBB 3.1 forces us to fix phpbb_root_path in get_user_avatar */
-		// 		s = s.replace(/\.\.\/\.\.\/download/g, 'download'); 
-		// 		$("#fltl").html( s );
-		// 	}
-		// });
 	});
 });
