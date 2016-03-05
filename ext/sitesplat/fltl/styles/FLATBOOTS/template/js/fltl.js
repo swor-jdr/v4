4ecdf7bd651da8230e8ed994477acfbed304db95
Tak Castel <takcastel@gmail.com>
2016-03-05 21:21:09 +0100

    test view more topics fix

--- ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
+++ ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
@@ -1,18 +1,25 @@
 head.ready(function () {
-	$(document).on("click", ".rtbutton", function () {
+	$(document).on("click", ".rtbutton", function (e) {
+
+		e.preventDefault();
+
 		var hack = new Date();
 	
-		$.ajax({
-			url: 'app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()),
-			
-			context: document.getElementById("fltl"),
-			error: function (e, text, ee) {
-			},
-			success: function (s, x) {
-		/* sucks, but a bug in phpBB 3.1 forces us to fix phpbb_root_path in get_user_avatar */
-				s = s.replace(/\.\.\/\.\.\/download/g, 'download'); 
-				$("#fltl").html( s );
-			}
+		$("#fltl").load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #fltl', function() {
+			console.log('ok');
 		});
+
+		// $.ajax({
+		// 	url: 'app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()),
+			
+		// 	context: document.getElementById("fltl"),
+		// 	error: function (e, text, ee) {
+		// 	},
+		// 	success: function (s, x) {
+		// /* sucks, but a bug in phpBB 3.1 forces us to fix phpbb_root_path in get_user_avatar */
+		// 		s = s.replace(/\.\.\/\.\.\/download/g, 'download'); 
+		// 		$("#fltl").html( s );
+		// 	}
+		// });
 	});
 });
