d449355b2b2c18f622c48f4fc487ea1c609e6569
Tak Castel <takcastel@gmail.com>
2016-03-06 12:42:58 +0100

    change ajax to load jquery method for view more topics

--- ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
+++ ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
@@ -4,10 +4,7 @@ head.ready(function () {
 		e.preventDefault();
 
 		var hack = new Date();
-	
-		$.ajax({url: 'app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()), async: true})
-			.done(function(data) {
-				$("#fltl").html(data);
-			});
+		
+		$('#fltl').load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()));
 	});
 });
