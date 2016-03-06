fc92245bf12ef8b971038dcc8900a8ddc33ba6e6
Tak Castel <takcastel@gmail.com>
2016-03-06 14:01:34 +0100

    change ajax jquery to show more topics

--- ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
+++ ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
@@ -2,8 +2,16 @@ head.ready(function () {
 	$(document).on("click", ".rtbutton", function (e) {
 		e.preventDefault();
 		var hack = new Date();
-		$('#fltl').load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #fltl > *, #script-fltl', function(data) {
-			console.log(data)
+		$('#fltl').load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #fltl > *', function(data) {
+			console.log(data);
+		});
+
+		if (!$('#script-fltl')) {
+			$('body').append('<script id="script-fltl"></script>')
+		}
+
+		$('#script-fltl').load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()) + ' #script-fltl', function(data) {
+			console.log(data);
 		});
 	});
 });
