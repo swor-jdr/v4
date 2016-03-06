955436b682a836315b3f468a6f3f9e8e83f73f19
Tak Castel <takcastel@gmail.com>
2016-03-06 12:52:54 +0100

    change method for view more topics

--- ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
+++ ext/sitesplat/fltl/styles/FLATBOOTS/template/js/fltl.js
@@ -1,10 +1,11 @@
 head.ready(function () {
 	$(document).on("click", ".rtbutton", function (e) {
-
 		e.preventDefault();
-
 		var hack = new Date();
-		
-		$('#fltl').load('app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime()));
+		$.ajax({url: 'app.php/fltl/fltl.html?fltl_start=' + start + '&time=' + String(hack.getTime())})
+			.done(function(data) {
+				console.log($(data));
+				$("#fltl").html($(data).find($('#fltl')));
+			});
 	});
 });
